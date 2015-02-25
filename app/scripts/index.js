3dd46d1650498adf4126f4c2c5a782a6714c0520
mollytaryn <mollytaryn@gmail.com>
2015-02-25 14:36:12 -0600

    added event target to game board

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -20,11 +20,36 @@ var fb = new Firebase('https://exandoh.firebaseio.com/'),
         username: $('#newUser').val()
       }
      addUser(createUsers, function(data){});
+     $('.welcomePlayers').attr('data-uuid', data.username);
   });
 
   function addUser(data, cb) {
     cb(fb.push(data));
   }
+
+//APPEND DATA TO PAGE
+function appendPlayersToPage(data) {
+  $('.welcomePlayers').append('<div> Welcome ' + data.username + '</div>');
+}
+
+//PULL DATA FROM FIREBASE
+fb.on('child_added', function(snap){
+  var data = snap.val();
+  appendPlayersToPage(data);
+});
+
+
+$('.table').on('click', function() {
+  // var $td = $($('tbody')).closest('td');
+  $('.output').html(event.target.className);
+  console.log("You have clicked on the table.")
+})
+
+  function createNewGame() {
+  }
+
+
+
 
   //if authenticated, go to app page
 
