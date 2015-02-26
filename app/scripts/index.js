9654d91281fc2f5620a641d6ae16dcb9a008e45e
mollytaryn <mollytaryn@gmail.com>
2015-02-26 14:25:10 -0600

    Game really, really working, seriously for reals

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -103,32 +103,61 @@ function findCellIndex(){
 }
 
 function turnCount() {
-  if (board[0] && board[1] && board[2] === "X") {
+  if ((board[0] === board[1] && board[0] === board[2] && board[0] === "X")
+  || (board[3] === board[4] && board[3] === board[5] && board[3] === "X")
+  || (board[6] === board[7] && board[6] === board[8] && board[6] === "X")
+  || (board[0] === board[3] && board[0] === board[6] && board[0] === "X")
+  || (board[1] === board[4] && board[1] === board[7] && board[1] === "X")
+  || (board[2] === board[5] && board[2] === board[8] && board[2] === "X")
+  || (board[0] === board[4] && board[0] === board[8] && board[0] === "X")
+  || (board[2] === board[4] && board[2] === board[6] && board[2] === "X")) {
     $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
-  } else if (board[3] && board[4] && board[5] === "X") {
-    $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
-  } else if (board[6] && board[7] && board[8] === "X") {
-    $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
-  } else if (board[0] && board[3] && board[6] === "X") {
-    $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
-  } else if (board[1] && board[4] && board[7] === "X") {
-    $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
-  } else if (board[2] && board[5] && board[8] === "X") {
-    $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
-  } else if (board[0] && board[4] && board[8] === "X") {
-    $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
-  } else if (board[2] && board[4] && board[6] === "X") {
-    $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
-  // } else if
-  //     ((board[0] && board[1] && board[2] === "O")
-  //   || (board[3] && board[4] && board[5] === "O")
-  //   || (board[6] && board[7] && board[8] === "O")
-  //   || (board[0] && board[3] && board[6] === "O")
-  //   || (board[1] && board[4] && board[7] === "O")
-  //   || (board[2] && board[5] && board[8] === "O")
-  //   || (board[0] && board[4] && board[8] === "O")
-  //   || (board[2] && board[4] && board[6] === "O")) {
+
+  } else if ((board[0] === board[1] && board[0] === board[2] && board[0] === "O")
+  || (board[3] === board[4] && board[3] === board[5] && board[3] === "O")
+  || (board[6] === board[7] && board[6] === board[8] && board[6] === "O")
+  || (board[0] === board[3] && board[0] === board[6] && board[0] === "O")
+  || (board[1] === board[4] && board[1] === board[7] && board[1] === "O")
+  || (board[2] === board[5] && board[2] === board[8] && board[2] === "O")
+  || (board[0] === board[4] && board[0] === board[8] && board[0] === "O")
+  || (board[2] === board[4] && board[2] === board[6] && board[2] === "O")) {
+    $('.clickOutput').append('<h1>PLAYER TWO WINS!!!</h1>');
+
+
+  // if (board[0] && board[1] && board[2] === "X") {
+    
+  // } else if (board[3] && board[4] && board[5] === "X") {
+  //   $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
+  // } else if (board[6] && board[7] && board[8] === "X") {
+  //   $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
+  // } else if (board[0] && board[3] && board[6] === "X") {
+  //   $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
+  // } else if (board[1] && board[4] && board[7] === "X") {
+  //   $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
+  // } else if (board[2] && board[5] && board[8] === "X") {
+  //   $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
+  // } else if (board[0] && board[4] && board[8] === "X") {
+  //   $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
+  // } else if (board[2] && board[4] && board[6] === "X") {
+  //   $('.clickOutput').append('<h1>PLAYER ONE WINS!!!</h1>');
+
+  // } else if(board[0] && board[1] && board[2] === "O") {
   //   $('.clickOutput').append('<h1>PLAYER TWO WINS!!!</h1>');
+  // } else if (board[3] && board[4] && board[5] === "O") {
+  //   $('.clickOutput').append('<h1>PLAYER TWO WINS!!!</h1>');
+  // } else if (board[6] && board[7] && board[8] === "O") {
+  //   $('.clickOutput').append('<h1>PLAYER TWO WINS!!!</h1>');
+  // } else if (board[0] && board[3] && board[6] === "O") {
+  //   $('.clickOutput').append('<h1>PLAYER TWO WINS!!!</h1>');
+  // } else if (board[1] && board[4] && board[7] === "O") {
+  //   $('.clickOutput').append('<h1>PLAYER TWO WINS!!!</h1>');
+  // } else if (board[2] && board[5] && board[8] === "O") {
+  //   $('.clickOutput').append('<h1>PLAYER TWO WINS!!!</h1>');
+  // } else if (board[0] && board[4] && board[8] === "O") {
+  //   $('.clickOutput').append('<h1>PLAYER TWO WINS!!!</h1>');
+  // } else if (board[2] && board[4] && board[6] === "O") {
+  //   $('.clickOutput').append('<h1>PLAYER TWO WINS!!!</h1>');
+
   } else if(turns === 9) {
     $('.clickOutput').append('<h1>GAME OVER!!!</h1>');
   }
